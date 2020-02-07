652486128895df66028be8f8e5aa9546c73630da
Shehryar Ehtesham <se318@rutgers.edu>
2020-02-07 18:50:44 -0500

    List.js

--- client/src/components/List/List.js
+++ client/src/components/List/List.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./List.css"
 
-export function List(props){
+export function List({ children }){
     return (
         <div className="list-overflow-container">
             <ul className="list-group">{children}</ul>

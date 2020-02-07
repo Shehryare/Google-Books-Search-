3ddcc8f0dd1ce0824861397480a0d0d9845e81e8
Shehryar Ehtesham <se318@rutgers.edu>
2020-02-06 23:31:27 -0500

    input.js

--- client/src/components/Form/FormBtn.js
+++ client/src/components/Form/FormBtn.js
@@ -4,7 +4,7 @@ export function FormBtn(props){
 
 return (
     <button {...props} style={{ float: "right", marginBottom: 10 }} className='btn btn-success'>
-        props.children
+        {props.children}
     </button>
 )
 };
